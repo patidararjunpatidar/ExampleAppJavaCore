508aa124cff815ee22a72f42dd411e16b68a2535
arjun <arjunpatidar952@gmail.com>
2018-03-04 10:56:31 +0530

    Arrays class example

---
+++ src/com/company/ArraysClassSorting.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ArraysClassSorting{
+    public static void main(String []args){
+
+        int[] array = {100,101,106,107,104};
+        System.out.println("Array Elements before sort");
+
+        for(int elem:array){
+            System.out.println(elem);
+        }
+        // call Arrays.sort on the int array.
+        Arrays.sort(array);
+        System.out.println("Array List After sort");
+        for(int elem:array){
+            System.out.println(elem);
+        }
+    }
+
+}
