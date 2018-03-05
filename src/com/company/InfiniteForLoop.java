ae920013fa5c5caea3bc9837d23c68cdeeb81b83
arjun <arjunpatidar952@gmail.com>
2018-03-05 23:40:08 +0530

    Infinite For Loop

---
+++ src/com/company/InfiniteForLoop.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class InfiniteForLoop {
+    public  static  void main(String []args){
+        for (;;){
+            System.out.println("Loop Get Called");
+        }
+
+    }
+}
