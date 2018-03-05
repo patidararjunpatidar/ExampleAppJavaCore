4a72c79102e9026121d6e879234a192d83d3dd82
arjun <arjunpatidar952@gmail.com>
2018-03-05 23:19:19 +0530

    Arrays Convert to String

---
+++ src/com/company/ArrayConvertToString.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ArrayConvertToString {
+
+    public static  void  main(String []args){
+
+        int[] arrays ={222,333,444};
+
+        //Call Arrays.toString to display the elements.
+
+        System.out.println(Arrays.toString(arrays));
+
+    }
+
+}
