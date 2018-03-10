34b1d732403a28396bead3f2bb720c285cc355e3
arjun <arjunpatidar952@gmail.com>
2018-03-10 17:23:46 +0530

    Scanner Class Example

---
+++ src/com/company/Scanner1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int a =input.nextInt();
+        System.out.println("Enter a Second Number");
+        int b =input.nextInt();
+        int c = a+b;
+        System.out.println("Sum is :"+ c);
+
+    }
+}
