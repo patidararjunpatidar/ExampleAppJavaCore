002f9f1b5116e5ba280234107b98b1c9010943c1
arjun <arjunpatidar952@gmail.com>
2018-03-10 18:27:59 +0530

    Reverse number using for loop

---
+++ src/com/company/ReverseNumberUsingForLoop.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ReverseNumberUsingForLoop {
+    public static void main(String[] args) {
+        int num=0,tempNum;
+        int reversenum=0;
+
+        System.out.println("input your number and press enter");
+
+        Scanner scanner= new Scanner(System.in);
+        tempNum = num=scanner.nextInt();
+
+        for (;num!=0;) {
+            reversenum = reversenum * 10;
+            reversenum = reversenum + num % 10;
+            num = num / 10;
+        }
+
+        System.out.println("Reverse number of : "+tempNum+" is "+reversenum);
+    }
+}
